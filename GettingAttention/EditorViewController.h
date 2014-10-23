5eedfac503e0aa058fa3290bc62fd7d54072b01b
Doysoft <lucklui@doysoft.com>
2014-10-23 10:53:55 +0800

    demo

---
+++ GettingAttention/EditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditorViewController.h
+//  GettingAttention
+//
+//  Created by Luck Lui on 28/8/14.
+//  Copyright (c) 2014 Rabeanco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditorViewController : UIViewController
+
+@end
--- GettingAttention/ViewController.m
+++ GettingAttention/ViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "ViewController.h"
+@import AudioToolbox;
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *userOutput;
+
+- (IBAction)doAlerts:(id)sender;
+- (IBAction)doAlertInput:(id)sender;
+- (IBAction)doActionSheet:(id)sender;
+- (IBAction)doSound:(id)sender;
+- (IBAction)doVibration:(id)sender;
 
 @end
 
@@ -25,5 +34,96 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)doAlerts:(id)sender {
+    UIAlertView *alertDialog;
+    alertDialog = [[UIAlertView alloc]
+                   initWithTitle:nil
+                   message:@"I need your attention NOW!"
+                   delegate:self
+                   cancelButtonTitle:@"OK"
+                   otherButtonTitles: @"Maybe Later",@"Never",nil
+                   
+                   
+                   ];
+    [alertDialog show];
+}
+
+- (IBAction)doAlertInput:(id)sender {
+    UIAlertView *alertDialog;
+    alertDialog = [[UIAlertView alloc]
+                   initWithTitle:@"Email Address"
+                   message:@"Please enter your email address"
+                   delegate:self
+                   cancelButtonTitle:@"OK"
+                   otherButtonTitles: nil];
+    
+    alertDialog.alertViewStyle=UIAlertViewStylePlainTextInput;
+    [alertDialog show];
+}
+
+- (IBAction)doActionSheet:(id)sender {
+    UIActionSheet *actionSheet;
+    actionSheet = [
+                  [UIActionSheet alloc]
+                   initWithTitle:@"Available Actions"
+                   delegate:self
+                   cancelButtonTitle:@"Cancel"
+                   destructiveButtonTitle:@"Destory"
+                   otherButtonTitles:@"Negotiate",@"Compromise", nil
+                  ];
+    actionSheet.actionSheetStyle=UIActionSheetStyleDefault;
+    [actionSheet showFromRect:[(UIButton *)sender frame]
+                       inView:self.view
+                        animated:YES];
+}
+
+- (IBAction)doSound:(id)sender {
+    SystemSoundID soundID;
+    NSString *soundFile = [
+                          [NSBundle mainBundle]
+                          pathForResource:@"soundeffect" ofType:@"wav"
+                          ];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:soundFile],&soundID);
+    AudioServicesPlaySystemSound(soundID);
+}
+
+- (IBAction)doVibration:(id)sender {
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+}
+- (void)alertView:(UIAlertView *)alertView
+clickedButtonAtIndex:(NSInteger)buttonIndex {
+    NSString *buttonTitle=[alertView buttonTitleAtIndex:buttonIndex];
+    if ([buttonTitle isEqualToString:@"Maybe Later"]) {
+        self.userOutput.text = @"Clicked 'Maybe Later'";
+    }else if ([buttonTitle isEqualToString:@"Never"]){
+        self.userOutput.text=@"Clicked 'Never'";
+    }else{
+        self.userOutput.text=@"Clicked 'OK'";
+    }
+    
+    if ([alertView.title isEqualToString:@"Email Address"]) {
+        self.userOutput.text=[[alertView textFieldAtIndex:0] text];
+    }
+}
+
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    NSString *buttonTitle=[actionSheet buttonTitleAtIndex:buttonIndex];
+    
+    if ([buttonTitle isEqualToString:@"Destory"]) {
+        self.userOutput.text = @"Clicked 'Destory'";
+    }else if ([buttonTitle isEqualToString:@"Negotiate"]){
+        self.userOutput.text=@"Clicked 'Negotiate'";
+    }else if([buttonTitle isEqualToString:@"Compromise"]){
+        self.userOutput.text=@"Clicked 'Compromise'";
+    }else{
+        self.userOutput.text=@"Clicked 'Cancel'";
+    }
+}
+
+
+
+
 
 @end
